cf1eb157ed446688536c39c59a1e6bf7c85dfe36
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 17:53:43 -0500

    se agregan dependencias en el pom.xml

---
+++ src/main/java/com/co/linadev/raul_hardware_backend/RaulHardwareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.co.linadev.raul_hardware_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaulHardwareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaulHardwareBackendApplication.class, args);
+    }
+
+}
