b799185bf73f81b48ed93818156a85eea8320dd4
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 18:00:45 -0500

    se crea collection Employee

---
+++ src/main/java/com/co/linadev/raul_hardware_backend/domain/collections/Employee.java
@@ -0,0 +1,17 @@
+package com.co.linadev.raul_hardware_backend.domain.collections;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(value="employees")
+public class Employee {
+
+    @Id
+    private final String id;
+    private final String name;
+    private final String phoneNumber;
+    private final String identityDocument;
+
+}
