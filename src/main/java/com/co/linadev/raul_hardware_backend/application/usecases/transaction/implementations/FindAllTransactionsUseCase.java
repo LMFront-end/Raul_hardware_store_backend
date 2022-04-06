21f879376f1da8b332e07abccb9ef9d92b793aec
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 20:13:43 -0500

    se crea usecase findAllTransaction

--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/transaction/implementations/FindAllTransactionsUseCase.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/transaction/implementations/FindAllTransactionsUseCase.java
@@ -1,9 +1,10 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.transaction.implementations;
 
-import com.example.raul_hardware_store.application.mappers.TransactionMapper;
-import com.example.raul_hardware_store.application.usecases.transaction.interfaces.FindAllTransactions;
-import com.example.raul_hardware_store.domain.dtos.TransactionDTO;
-import com.example.raul_hardware_store.domain.repositories.TransactionRepository;
+
+import com.co.linadev.raul_hardware_backend.application.mappers.TransactionMapper;
+import com.co.linadev.raul_hardware_backend.application.usecases.transaction.interfaces.FindAllTransactions;
+import com.co.linadev.raul_hardware_backend.domain.dtos.TransactionDTO;
+import com.co.linadev.raul_hardware_backend.domain.repositories.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/transaction/interfaces/FindAllTransactions.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/transaction/interfaces/FindAllTransactions.java
@@ -1,6 +1,7 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.transaction.interfaces;
 
-import com.example.raul_hardware_store.domain.dtos.TransactionDTO;
+
+import com.co.linadev.raul_hardware_backend.domain.dtos.TransactionDTO;
 import reactor.core.publisher.Flux;
 
 @FunctionalInterface
