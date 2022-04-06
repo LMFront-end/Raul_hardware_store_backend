dcec0cbfc506fbb196564bb69dd441ab8e0a36c0
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 19:42:19 -0500

    se crea usecase FindAllBills

--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/bill/implementations/FindAllBillsUseCase.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/bill/implementations/FindAllBillsUseCase.java
@@ -1,12 +1,14 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.bill.implementations;
 
-import com.example.raul_hardware_store.application.mappers.BillMapper;
-import com.example.raul_hardware_store.application.usecases.bill.interfaces.FindAllBills;
-import com.example.raul_hardware_store.domain.dtos.BillDTO;
-import com.example.raul_hardware_store.domain.repositories.BillRepository;
+
+import com.co.linadev.raul_hardware_backend.application.mappers.BillMapper;
+import com.co.linadev.raul_hardware_backend.application.usecases.bill.interfaces.FindAllBills;
+import com.co.linadev.raul_hardware_backend.domain.dtos.BillDTO;
+import com.co.linadev.raul_hardware_backend.domain.repositories.BillRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/bill/interfaces/FindAllBills.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/bill/interfaces/FindAllBills.java
@@ -1,6 +1,7 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.bill.interfaces;
 
-import com.example.raul_hardware_store.domain.dtos.BillDTO;
+
+import com.co.linadev.raul_hardware_backend.domain.dtos.BillDTO;
 import reactor.core.publisher.Flux;
 
 @FunctionalInterface
