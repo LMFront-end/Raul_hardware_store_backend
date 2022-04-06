21847ad8c3185a6fc7b0f01ec6838190044d6983
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 19:31:05 -0500

    se crea EmployeeMapper

---
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/mappers/EmployeeMapper.java
@@ -0,0 +1,28 @@
+package com.co.linadev.raul_hardware_backend.application.mappers;
+
+import com.co.linadev.raul_hardware_backend.domain.collections.Employee;
+import com.co.linadev.raul_hardware_backend.domain.dtos.EmployeeDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class EmployeeMapper {
+
+    public Function<Employee, EmployeeDTO> mapToDto(){
+        return employee -> new EmployeeDTO(employee.getId(),
+                employee.getName(),
+                employee.getPhoneNumber(),
+                employee.getIdentityDocument()
+        );
+    }
+
+    public Function<EmployeeDTO, Employee> mapToCollection(){
+        return employeeDTO -> new Employee(employeeDTO.getId(),
+                employeeDTO.getName(),
+                employeeDTO.getPhoneNumber(),
+                employeeDTO.getIdentityDocument()
+        );
+    }
+
+}
