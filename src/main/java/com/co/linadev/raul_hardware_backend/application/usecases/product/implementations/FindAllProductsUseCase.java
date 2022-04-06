22bf46a079706d2aa89f5771d929531deecf5edc
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-05 20:05:04 -0500

    se crea usecase findAllProductsProduct

--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/product/implementations/FindAllProductsUseCase.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/product/implementations/FindAllProductsUseCase.java
@@ -1,9 +1,10 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.product.implementations;
 
-import com.example.raul_hardware_store.application.mappers.ProductMapper;
-import com.example.raul_hardware_store.application.usecases.product.interfaces.FindAllProducts;
-import com.example.raul_hardware_store.domain.dtos.ProductDTO;
-import com.example.raul_hardware_store.domain.repositories.ProductRepository;
+
+import com.co.linadev.raul_hardware_backend.application.mappers.ProductMapper;
+import com.co.linadev.raul_hardware_backend.application.usecases.product.interfaces.FindAllProducts;
+import com.co.linadev.raul_hardware_backend.domain.dtos.ProductDTO;
+import com.co.linadev.raul_hardware_backend.domain.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
--- src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/product/interfaces/FindAllProducts.java
+++ src/main/java/com/co/linadev/raul_hardware_backend/application/usecases/product/interfaces/FindAllProducts.java
@@ -1,6 +1,7 @@
 package com.co.linadev.raul_hardware_backend.application.usecases.product.interfaces;
 
-import com.example.raul_hardware_store.domain.dtos.ProductDTO;
+
+import com.co.linadev.raul_hardware_backend.domain.dtos.ProductDTO;
 import reactor.core.publisher.Flux;
 
 @FunctionalInterface
